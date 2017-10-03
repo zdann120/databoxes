f704c51a4ae0734440ee8ae8d6abf82bccb562bd
Zach Nusbaum <zachdnusbaum@gmail.com>
2017-10-02 19:28:46 -0700

    handle Pundit::Unauthorized exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
   include Pundit
+
+  private
+
+  def not_authorized
+    render plain: 'Sorry, you cannot view this.'
+  end
 end
