ef1633884cf322f530d5954b67b640517c3b04d5
Zach Nusbaum <zachdnusbaum@gmail.com>
2017-10-02 19:33:17 -0700

    fix datum policy

--- app/policies/datum_policy.rb
+++ app/policies/datum_policy.rb
@@ -1,10 +1,10 @@
 class DatumPolicy < ApplicationPolicy
   def show?
-    (record.box.user == user) || user.admin? || record.box.public?
+    (record.box.user == user) || user.try(:admin?) || record.box.public?
   end
 
   def payload?
-    (record.box.user == user) || user.admin? || record.box.public?
+    (record.box.user == user) || user.try(:admin?) || record.box.public?
   end
   class Scope < Scope
     def resolve
